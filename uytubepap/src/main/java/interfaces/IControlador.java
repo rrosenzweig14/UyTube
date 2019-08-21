86b40ae9d7ca0c2de183125e45558e19f111603a
Fabio <kigoromous@gmail.com>
2019-08-21 20:28:39 -0300

    Add Controlador and IControlador

---
+++ uytubepap/src/main/java/interfaces/IControlador.java
@@ -0,0 +1,52 @@
+package interfaces;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
+
+import datatypes.DtCategoria;
+import datatypes.DtComentario;
+import datatypes.DtLista;
+import datatypes.DtVideo;
+
+public interface IControlador {
+	
+	public void ValorarVideo(String nick, boolean valor);
+	
+	public ArrayList<DtVideo> VideoEnLista(DtLista lst);
+	
+	public DtCategoria SeleccionarCategoria(String cat);
+	
+	public DtComentario seleccionarComentario(DtComentario comment);
+	
+	public void seguirUsuario(DtUsuario usr1, DtUsuario usr2);
+	
+	public void quitarVideo(DtVideo video);
+	
+	public ArrayList<DtComentario> MostrarComentario();
+	
+	public void modificarUsuarioCanal(DtUsuario usr, DtCanal canal);
+	
+	public void IngresarComentario(DtComentario comment);
+	
+	public DtVideo seleccionarVideo(String video);
+	
+	public DtUsuario seleccionarUsuario(String usuario);
+		
+	public ArrayList<DtVideo> listarVideos();
+	
+	public ArrayList<DtUsuario> listarUsuarios();
+	
+	public void ingresarVideo(String nombre, String duaracion, String url, String descripcion, Date fecha, DtCategoria categoria);
+
+	public void editarVideo(DtVideo video);
+	
+	public void agregarVideo(String video, DtLista lista);
+	
+	public boolean crearLista(DtUsuario usuario, String nombre, boolean privada, String categoria);
+	
+	public Map<ArrayList<DtVideo>, ArrayList<DtLista>> listarXCat(String categoria);
+	
+	public Map<DtUsuario, DtCanal> listarDatosUsuario(String nick);
+	
+}
---
+++ uytubepap/src/main/java/logica/Controlador.java
@@ -0,0 +1,131 @@
+package logica;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Map;
+
+import datatypes.DtCategoria;
+import datatypes.DtComentario;
+import datatypes.DtLista;
+import datatypes.DtVideo;
+import interfaces.IControlador;
+
+public class Controlador implements IControlador{
+
+	@Override
+	public void ValorarVideo(String nick, boolean valor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<DtVideo> VideoEnLista(DtLista lst) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DtCategoria SeleccionarCategoria(String cat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DtComentario seleccionarComentario(DtComentario comment) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void seguirUsuario(DtUsuario usr1, DtUsuario usr2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void quitarVideo(DtVideo video) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<DtComentario> MostrarComentario() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void modificarUsuarioCanal(DtUsuario usr, DtCanal canal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void IngresarComentario(DtComentario comment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DtVideo seleccionarVideo(String video) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DtUsuario seleccionarUsuario(String usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<DtVideo> listarVideos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<DtUsuario> listarUsuarios() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void ingresarVideo(String nombre, String duaracion, String url, String descripcion, Date fecha,
+			DtCategoria categoria) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void editarVideo(DtVideo video) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void agregarVideo(String video, DtLista lista) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean crearLista(DtUsuario usuario, String nombre, boolean privada, String categoria) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Map<ArrayList<DtVideo>, ArrayList<DtLista>> listarXCat(String categoria) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<DtUsuario, DtCanal> listarDatosUsuario(String nick) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
